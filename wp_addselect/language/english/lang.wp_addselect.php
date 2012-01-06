0e56eda98697810aeac1891023940ee00682654e
John Wee Pixel <john@weepixel.com>
2012-01-06 08:21:44 +0000

    Added necessaries to lang file

---
+++ wp_addselect/language/english/lang.wp_addselect.php
@@ -0,0 +1,8 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang = array(
+	
+);
+
+/* End of file lang.wp_addselect.php */
+/* Location: /system/expressionengine/third_party/wp_addselect/language/english/lang.wp_addselect.php */
